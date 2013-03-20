3b83f9cfe80bce0b7751b644e42fecc3e8a35b51
Jonathan Moore Liles <j.liles@unix.net>
2013-03-19 17:04:05 -0700

    Use NTK instead of FLTK.

--- compile_linux_fftw_jack.sh
+++ compile_linux_fftw_jack.sh
@@ -2,11 +2,11 @@ outfile=paulstretch
 
 rm -f $outfile
 
-fluid -c GUI.fl 
-fluid -c FreeEditUI.fl
+ntk-fluid -c GUI.fl 
+ntk-fluid -c FreeEditUI.fl
 
-g++ -ggdb GUI.cxx FreeEditUI.cxx *.cpp Input/*.cpp Output/*.cpp `fltk-config --cflags` \
- `fltk-config --ldflags` \
+g++ -ggdb GUI.cxx FreeEditUI.cxx *.cpp Input/*.cpp Output/*.cpp `pkg-config ntk --cflags` \
+ `pkg-config ntk --libs` \
  -laudiofile -lfftw3f -lvorbisenc -lvorbisfile -lportaudio -lpthread -lmad -lmxml \
  `pkg-config --cflags --libs jack samplerate` \
  -DHAVE_JACK -DENABLE_RESAMPLING \
